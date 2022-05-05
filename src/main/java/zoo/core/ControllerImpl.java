6536f0b1910b66999cbbda4abbc47f8489153fe5
diana851011 <borisova.diana@abv.bg>
2022-05-05 22:07:34 +0300

    add classes

--- src/main/java/zoo/core/ControllerImpl.java
+++ src/main/java/zoo/core/ControllerImpl.java
@@ -1,19 +1,63 @@
 package zoo.core;
 
-public class ControllerImpl implements Controller {
+import zoo.common.ConstantMessages;
+import zoo.common.ExceptionMessages;
+import zoo.entities.LandArea;
+import zoo.entities.Meat;
+import zoo.entities.Vegetable;
+import zoo.entities.WaterArea;
+import zoo.entities.areas.Area;
+import zoo.entities.foods.Food;
+import zoo.repositories.FoodRepository;
+import zoo.repositories.FoodRepositoryImpl;
 
-    @Override
-    public String addArea(String areaType, String areaName) {
-        return null;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ControllerImpl implements Controller {
+    private FoodRepository foodRepository;
+    private Collection<Area>areas;
+
+    public ControllerImpl() {
+        this.foodRepository = new FoodRepositoryImpl();
+        this.areas = new ArrayList<>();
     }
 
     @Override
+    public String addArea(String areaType, String areaName) {
+        Area area = null;
+        if (areaType.equals("WaterArea")) {
+            area = new WaterArea(areaName);
+        } else if (areaType.equals("LandArea")){
+            area = new LandArea(areaName);
+
+        } else {
+            throw new NullPointerException(String.format(ExceptionMessages.INVALID_AREA_TYPE));
+        }
+            return String.format(ConstantMessages.SUCCESSFULLY_ADDED_AREA_TYPE, areaType);
+    }
+    @Override
     public String buyFood(String foodType) {
-        return null;
+        Food food = null;
+        if (foodType.equals("Vegetable")){
+            food = new Vegetable();
+        } else if (foodType.equals("Meat")){
+    food= new Meat();
+        } else {
+            throw new IllegalArgumentException(ExceptionMessages.INVALID_FOOD_TYPE);
+        }
+        this.foodRepository.add(food);
+
+        return String.format(ConstantMessages.SUCCESSFULLY_ADDED_FOOD_TYPE, foodType);
     }
 
     @Override
     public String foodForArea(String areaName, String foodType) {
+       if (this.foodRepository.findByType(foodType) != null &&
+               (areaName.equals("WaterArea") || areaName.equals("LandArea"))){
+           Food food = this.foodRepository.findByType(foodType);
+           this.foodRepository.remove(food);
+       }
         return null;
     }
 
